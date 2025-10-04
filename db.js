5a14bf2c054fa84ff9edda96b66f361cdbbba2ce
aptapansary2-max <aptapansary2@gmail.com>
2025-10-04 14:09:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -7,12 +7,16 @@ async function initDatabase() {
   try {
     pool = mysql.createPool({
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       waitForConnections: true,
       connectionLimit: 10,
-      queueLimit: 0
+      queueLimit: 0,
+      connectTimeout: 30000,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 0
     });
 
     // Create tables if not exist
